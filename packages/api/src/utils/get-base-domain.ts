7d3bd150e5c0063639510301ef3a49aa0b15d728
parasSolanki <parassolanki151@gmail.com>
2023-10-03 19:45:32 +0530

    add: redirect to referer domain after google signin

--- packages/api/src/utils/get-base-domain.ts
+++ packages/api/src/utils/get-base-domain.ts
@@ -1,8 +1,8 @@
 /**
- *
+ * Get base domain form url
  * @see https://github.com/lucia-auth/lucia/blob/main/packages/lucia/src/utils/url.ts
  */
-const getBaseDomain = (host: string): string => {
+export const getBaseDomain = (host: string): string => {
   if (host.startsWith('localhost:')) return host;
   return host.split('.').slice(-2).join('.');
 };
