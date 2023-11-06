5fa71a512a344ac41608d129ce4a4f32ef611377
parasSolanki <parassolanki151@gmail.com>
2023-11-06 22:43:04 +0530

    add: default role and user on seed

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,23 +1,63 @@
 import { PrismaClient } from '@prisma/client'
+import { createKeyId } from 'lucia'
+import { generateLuciaPasswordHash } from 'lucia/utils'
+import 'lucia/polyfill/node'
 
 const prisma = new PrismaClient()
 
-const ROLE = {
-  ADMIN: 'ADMIN',
-  USER: 'USER',
-}
-
 async function seed() {
   // seed default roles
-  await Promise.all(
-    Object.values(ROLE).map((role) =>
-      prisma.role.create({
-        data: {
-          name: role,
+  const adminRole = await prisma.role.create({
+    data: {
+      name: 'ADMIN',
+    },
+  })
+
+  const userRole = await prisma.role.create({
+    data: {
+      name: 'USER',
+    },
+  })
+
+  console.log(`Roles data has been seeded. 🌱`)
+
+  await prisma.user.create({
+    data: {
+      email: 'admin@mail.com',
+      name: 'Admin',
+      role: {
+        connect: {
+          id: adminRole.id,
         },
-      })
-    )
-  )
+      },
+      key: {
+        create: {
+          id: createKeyId('email', 'admin@mail.com'),
+          hashed_password: await generateLuciaPasswordHash('12345678'),
+        },
+      },
+    },
+  })
+
+  await prisma.user.create({
+    data: {
+      email: 'user@mail.com',
+      name: 'User',
+      role: {
+        connect: {
+          id: userRole.id,
+        },
+      },
+      key: {
+        create: {
+          id: createKeyId('email', 'user@mail.com'),
+          hashed_password: await generateLuciaPasswordHash('12345678'),
+        },
+      },
+    },
+  })
+
+  console.log(`Users data has been seeded. 🌱`)
 
   console.log(`Database has been seeded. 🌱`)
 }
